95d7646c9bfccb94bbc1d39bf721cabfec5b1085
KamilSzat <uT_3776@zsp3zamosc.pl>
2024-10-17 09:17:37 +0200

    lista filmow

---
+++ src/MovieList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './App.css';
+
+
+
+
+const movies = [
+    {
+        title: "Zaczarowani",
+        genre:"familijny/rodzinny",
+        times: ["15:30", "18:35"],
+        premiere: true
+    },
+    {
+        title: "Zaplątani",
+        genre:"familijny/rodzinny",
+        times: ["12:30", "15:35", "18:35"],
+        premiere: true
+    },
+    {
+        title: "Wróżkowie chrzestni/doubing",
+        genre:"familijny/rodzinny",
+        times: ["15:30", "18:35"],
+        premiere: true
+    },
+    {
+        title: "Wyfrunięci",
+        genre:"familijny/rodzinny",
+        times: ["15:30", "18:35"],
+        premiere: true
+    },
+    {
+        title: "Matka siedzi z tyłu",
+        genre:"historyczny/dokumentalny",
+        times: ["11:00", "14:40"],
+        premiere: true
+    }
+];
+
+export default MoviesList;
